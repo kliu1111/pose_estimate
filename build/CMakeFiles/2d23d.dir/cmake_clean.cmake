278a6f4f9defb86c7f5f5659d9326fe9b53a7384
kliu1111 <2521105276@qq.com>
2017-11-11 20:27:58 +0800

    2d23d

---
+++ build/CMakeFiles/2d23d.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/2d23d.dir/2d23d.cpp.o"
+  "2d23d.pdb"
+  "2d23d"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/2d23d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
